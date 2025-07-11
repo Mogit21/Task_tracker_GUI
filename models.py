b8e26c19992c36e6bed966a037dd0bf75390b20f
Mohamed Haziane <Mohamed.Haziane@corp.eastlink.ca>
2025-07-11 15:54:11 -0300

    commit

--- models.py
+++ models.py
@@ -1,6 +1,3 @@
-<<<<<<< HEAD
-# SQLAlchemy models
-
 from sqlalchemy import Column, Integer, String, Date, ForeignKey, CheckConstraint
 from db import Base
 
@@ -18,22 +15,3 @@ class Task(Base):
     priority = Column(String, CheckConstraint("priority IN ('High', 'Medium', 'Low')"), nullable=False)
     deadline = Column(Date, nullable=False)
     status = Column(String, CheckConstraint("status IN ('To Do', 'In Progress', 'Done')"), nullable=False)
-=======
-from sqlalchemy import Column, Integer, String, Date, ForeignKey, CheckConstraint
-from db import Base
-
-class Category(Base):
-    __tablename__ = 'category'
-    id = Column(Integer, primary_key=True)
-    main_category = Column(String, nullable=False)
-    sub_category = Column(String, nullable=False)
-
-class Task(Base):
-    __tablename__ = 'task'
-    id = Column(Integer, primary_key=True)
-    category_id = Column(Integer, ForeignKey('category.id'), nullable=False)
-    description = Column(String, nullable=False)
-    priority = Column(String, CheckConstraint("priority IN ('High', 'Medium', 'Low')"), nullable=False)
-    deadline = Column(Date, nullable=False)
-    status = Column(String, CheckConstraint("status IN ('To Do', 'In Progress', 'Done')"), nullable=False)
->>>>>>> 04823a85d5ccb63ae834837a2cebb3c79d684e24
--- task_tracker_app_clean/task_tracker_app/db.py
+++
@@ -1,8 +0,0 @@
-import os
-from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker, declarative_base
-
-os.makedirs("data", exist_ok=True)
-Base = declarative_base()
-engine = create_engine("sqlite:///data/task_tracker.db")
-SessionLocal = sessionmaker(bind=engine)
